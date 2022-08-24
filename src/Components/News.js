02b8874c179ec0c6611bdfa1ce14e3cdf8746f55
kp-krish <kpofficial2004@gmail.com>
2022-08-24 11:27:47 +0530

    commit 1

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import NewsItems from './NewsItems'
 
 export class news extends Component {
+      articles = []
+    constructor(){
+      super();
+      console.log("Hello I am a constructor from News component");
+      this.state = {
+          articles: this.articles,
+          loading: false
+      }
+    } 
+    async componentDidMount(){
+      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=994e1c5fa8da4377a3b2f68413836e67"
+      let data=await fetch(url);
+      let parsedData= await data.json();
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles})
+    }
   render() {
+
     return (
-      <div>
-        This is a news component
+      <div className='container my-3'>
+        <h2>NewsKing - Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+          return <div className="col md-4 mt-4"  key={element.url}>
+              <NewsItems title={element.title?element.title.slice(0,40)+"...":"\n\n"} description={element.description?element.description.slice(0,60)+"...":""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            </div>
+        })}
+        </div>
       </div>
     )
   }
---
+++ src/Components/NewsItems.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div>
+        <div className="card" style={{width: "19rem"}}>
+        <img src={imageUrl?imageUrl:"https://images.indianexpress.com/2022/07/Untitled-design-17-1.jpg"} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItems
