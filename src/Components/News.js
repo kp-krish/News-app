9fea93707884eedb050fcffae9a3e03c4f4701af
kp-krish <kpofficial2004@gmail.com>
2022-08-24 12:17:59 +0530

    commit 2

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,18 +8,42 @@ export class news extends Component {
       console.log("Hello I am a constructor from News component");
       this.state = {
           articles: this.articles,
-          loading: false
+          loading: false,
+          page: 1
       }
     } 
     async componentDidMount(){
-      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=994e1c5fa8da4377a3b2f68413836e67"
+      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=994e1c5fa8da4377a3b2f68413836e67&page=1&pageSize=18"
       let data=await fetch(url);
       let parsedData= await data.json();
       console.log(parsedData);
-      this.setState({articles: parsedData.articles})
+      this.setState({articles: parsedData.articles, totalResults:parsedData.totalResults} )
+    }
+    handlePrevClick = async ()=>{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=18`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({
+          page: this.state.page - 1,
+          articles: parsedData.articles
+      })
+    }
+    handleNextClick = async ()=>{
+      if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+      }
+      else {
+          let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=18`;
+          let data = await fetch(url);
+          let parsedData = await data.json()
+          console.log(parsedData);
+          this.setState({
+              page: this.state.page + 1,
+              articles: parsedData.articles
+          })
+      }
     }
   render() {
-
     return (
       <div className='container my-3'>
         <h2>NewsKing - Top Headlines</h2>
@@ -29,6 +53,10 @@ export class news extends Component {
               <NewsItems title={element.title?element.title.slice(0,40)+"...":"\n\n"} description={element.description?element.description.slice(0,60)+"...":""} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
         })}
+        <div className="container d-flex justify-content-between mt-4">
+          <button type='button' className='btn btn-dark' disabled={this.state.page<=1} onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
         </div>
       </div>
     )
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -10,7 +10,7 @@ export class NewsItems extends Component {
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
+                <a  rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Go somewhere</a>
             </div>
         </div>
       </div>
