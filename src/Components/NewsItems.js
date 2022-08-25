58309513c55c69b6e20b976b19427264f2522780
kp-krish <kpofficial2004@gmail.com>
2022-08-25 15:32:23 +0530

    commit 4

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -5,12 +5,12 @@ export class NewsItems extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
-        <div className="card" style={{width: "19rem"}}>
+        <div className="card" style={{width: '18rem' ,marginLeft:'30px'}}>
         <img src={imageUrl?imageUrl:"https://images.indianexpress.com/2022/07/Untitled-design-17-1.jpg"} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a  rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Go somewhere</a>
+                <a  rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Read More</a>
             </div>
         </div>
       </div>
